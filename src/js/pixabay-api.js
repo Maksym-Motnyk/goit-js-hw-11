8af31041af7c2d3ec0bf5366aac6439e1fdf607b
Maksym Motnyk <maksummotnuk@gmail.com>
2024-05-14 15:44:40 +0300

    push

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,19 @@
+function showLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'block';
+}
+
+export function searchPhotos(input) {
+  showLoader();
+
+  const searchParams = new URLSearchParams({
+    key: '43803497-a801e9cfe7ea9bdd19d306bb3',
+    q: input.value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+
+  return fetch(url);
+}
